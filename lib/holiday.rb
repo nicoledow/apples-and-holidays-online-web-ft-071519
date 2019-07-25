42c738d1e66bb679a1adb2d72ad4f31c85abe47a
Nicole Dow <nikki.dow71@gmail.com>
2019-07-25 19:22:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,12 +66,12 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-   holiday_hash.each do |season, holiday_list|
+   uts "  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supply_array.join(", ")}"
+    end
+  endholiday_hash.each do |season, holiday_list|
      puts "#{season.to_s.capitalize}:"
    holiday_list.each do |holiday, supply_array|
-     puts "  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supply_array.join(", ")}"
-    end
-  end
+     p
  end
 
 def all_holidays_with_bbq(holiday_hash)
